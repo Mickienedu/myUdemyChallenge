2f5a72218630f476b72777734857addfbad1f381
Michael Okorie <mickienedu@gmail.com>
2023-12-13 12:25:02 +0100

    My other commit

--- FUNDAMENTAL3/data-structure-strings-operators/WORKING-ARRAYS/ADVANCED-DOM-AND-EVENTS/script.js
+++ FUNDAMENTAL3/data-structure-strings-operators/WORKING-ARRAYS/ADVANCED-DOM-AND-EVENTS/script.js
@@ -212,7 +212,7 @@ const allSections = document.querySelectorAll('.section')
 
 const revealSection = function(entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if(!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -225,124 +225,128 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 })
 allSections.forEach(function(section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-})
+  // section.classList.add('section--hidden');
+});
 
 
 // ///////////////////////////////////////
 // 199. Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src');
 
-// // Lazy loading images
-// const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
 
-// const loadImg = function (entries, observer) {
-//   const [entry] = entries;
+  if(!entry.isIntersecting) return;
 
-//   if (!entry.isIntersecting) return;
+  // replace src with data-src 
+  entry.target.src = entry.target.dataset.src;
 
-//   // Replace src with data-src
-//   entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
 
-//   entry.target.addEventListener('load', function () {
-//     entry.target.classList.remove('lazy-img');
-//   });
+  });
 
-//   observer.unobserve(entry.target);
-// };
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+})
 
-// const imgObserver = new IntersectionObserver(loadImg, {
-//   root: null,
-//   threshold: 0,
-//   rootMargin: '200px',
-// });
+imgTargets.forEach(img => imgObserver.observe(img))
 
-// imgTargets.forEach(img => imgObserver.observe(img));
 
 // ///////////////////////////////////////
-// // Slider
-// const slider = function () {
-//   const slides = document.querySelectorAll('.slide');
-//   const btnLeft = document.querySelector('.slider__btn--left');
-//   const btnRight = document.querySelector('.slider__btn--right');
-//   const dotContainer = document.querySelector('.dots');
+// 200. Building a Slider Component: Part 1
+// // SLIDER 
+// 201. Building a Slider Component: Part 2
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
 
-//   let curSlide = 0;
-//   const maxSlide = slides.length;
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-//   // Functions
-//   const createDots = function () {
-//     slides.forEach(function (_, i) {
-//       dotContainer.insertAdjacentHTML(
-//         'beforeend',
-//         `<button class="dots__dot" data-slide="${i}"></button>`
-//       );
-//     });
-//   };
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
 
-//   const activateDot = function (slide) {
-//     document
-//       .querySelectorAll('.dots__dot')
-//       .forEach(dot => dot.classList.remove('dots__dot--active'));
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
 
-//     document
-//       .querySelector(`.dots__dot[data-slide="${slide}"]`)
-//       .classList.add('dots__dot--active');
-//   };
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
 
-//   const goToSlide = function (slide) {
-//     slides.forEach(
-//       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-//     );
-//   };
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
 
-//   // Next slide
-//   const nextSlide = function () {
-//     if (curSlide === maxSlide - 1) {
-//       curSlide = 0;
-//     } else {
-//       curSlide++;
-//     }
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
 
-//     goToSlide(curSlide);
-//     activateDot(curSlide);
-//   };
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
 
-//   const prevSlide = function () {
-//     if (curSlide === 0) {
-//       curSlide = maxSlide - 1;
-//     } else {
-//       curSlide--;
-//     }
-//     goToSlide(curSlide);
-//     activateDot(curSlide);
-//   };
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
 
-//   const init = function () {
-//     goToSlide(0);
-//     createDots();
+  const init = function () {
+    goToSlide(0);
+    createDots();
 
-//     activateDot(0);
-//   };
-//   init();
+    activateDot(0);
+  };
+  init();
 
-//   // Event handlers
-//   btnRight.addEventListener('click', nextSlide);
-//   btnLeft.addEventListener('click', prevSlide);
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
 
-//   document.addEventListener('keydown', function (e) {
-//     if (e.key === 'ArrowLeft') prevSlide();
-//     e.key === 'ArrowRight' && nextSlide();
-//   });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
 
-//   dotContainer.addEventListener('click', function (e) {
-//     if (e.target.classList.contains('dots__dot')) {
-//       const { slide } = e.target.dataset;
-//       goToSlide(slide);
-//       activateDot(slide);
-//     }
-//   });
-// };
-// slider();
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+
 
 
 // 185. How the DOM Really Works
@@ -517,6 +521,22 @@ console.log(hOne.parentElement.children);
 // creating nice effect when you hover the links
 
 
+// 202. Lifecycle DOM Events
+document.addEventListener('DOMContentLoaded', function(e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function(e) {
+  console.log('Page fully loaded', e);
+})
+
+// window.addEventListener('beforeunload', function(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// })
+
+// 203. Efficient Script Loading: defer and async
 
 
 
